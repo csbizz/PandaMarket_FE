41d69a0c69065ad41ff13174bbd46bcd97143fd8
csbizz <95736373+csbizz@users.noreply.github.com>
2024-10-22 18:36:35 +0900

    Add articleDetail title part

--- pages/articles/[id].jsx
+++ pages/articles/[id].jsx
@@ -1,16 +1,106 @@
 /** @jsxImportSource @emotion/react */
 import { css } from '@emotion/react';
+import Image from 'next/image';
 
 const style = {
   articleDetail: css`
     padding-top: 3.4rem;
+
+    #article {
+      #articleHeader {
+        border-bottom: 1px solid var(--gray-200);
+      }
+
+      #title {
+        display: flex;
+        justify-content: space-between;
+
+        h1 {
+          font-size: 2rem;
+          line-height: 3.2rem;
+          font-weight: 700;
+          color: var(--gray-800);
+        }
+      }
+
+      #info {
+        margin-top: 1.6rem;
+        margin-bottom: 1.6rem;
+        display: inline-flex;
+        gap: 3.2rem;
+
+        > div {
+          display: flex;
+          align-items: center;
+
+          .nickname {
+            margin-left: 1.6rem;
+            margin-right: 0.8rem;
+
+            font-size: 1.4rem;
+            line-height: 2.4rem;
+            font-weight: 500;
+            color: var(--gray-600);
+          }
+
+          .date {
+            font-size: 1.4rem;
+            line-height: 2.4rem;
+            font-weight: 400;
+            color: var(--gray-400);
+          }
+        }
+
+        hr {
+          border-color: var(--gray-200);
+        }
+
+        button {
+          padding: 0.4rem 1.2rem;
+          border: 1px solid var(--gray-200);
+          border-radius: 35px;
+          display: flex;
+          align-items: center;
+
+          span {
+            font-size: 1.6rem;
+            line-height: 2.6rem;
+            font-weight: 500;
+            color: var(--gray-500);
+
+            margin-left: 0.4rem;
+          }
+        }
+      }
+    }
   `,
 };
 
 export default function ArticleDetail() {
   return (
     <div id="articleDetail" css={style.articleDetail}>
-      <div>asdf</div>
+      <div id="article">
+        <div id="articleHeader">
+          <div id="title">
+            <h1>맥북 16인치 16기가 1테라 정도 사양이면 얼마에 팔아야하나요?</h1>
+            <button type="button" className="button">
+              button
+            </button>
+          </div>
+          <div id="info">
+            <div>
+              <Image src="/Image/ic_profile.png" alt="profile image" width={40} height={40} />
+              <span className="nickname">총명한판다</span>
+              <span className="date">2024. 01. 02</span>
+            </div>
+            <hr />
+            <button type="button">
+              <img src="/Image/ic_heart.svg" alt="heart" width={32} height={32} />
+              <span>123</span>
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
--- src/components/SortOrderSelect.jsx
+++ src/components/SortOrderSelect.jsx
@@ -1,6 +1,6 @@
 /** @jsxImportSource @emotion/react */
 import { css } from '@emotion/react';
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { useViewport } from '../contexts/ViewportContext.jsx';
 import c from '../utils/constants.js';
 import Image from 'next/image';
@@ -34,6 +34,7 @@ const style = {
 };
 
 export default function SortOrderSelect({ initialSortOrder = c.SORT_ORDER.RECENT, onChange }) {
+  const dropdownRef = useRef();
   const viewport = useViewport();
   const [dropdownOpen, setDropdownOpen] = useState(false);
   const [sortOrder, setSortOrder] = useState(initialSortOrder);
@@ -45,8 +46,18 @@ export default function SortOrderSelect({ initialSortOrder = c.SORT_ORDER.RECENT
   };
   const toggleDropdown = () => setDropdownOpen(!dropdownOpen);
 
+  useEffect(() => {
+    const handleClickOutside = e => {
+      // NOTE Ref의 current에 담긴 엘리먼트가 아닌 곳(바깥)을 클릭 시 드롭다운 메뉴 닫힘
+      if (dropdownOpen && !dropdownRef.current.contains(e.target)) setDropdownOpen(false);
+    };
+    document.addEventListener('click', handleClickOutside);
+
+    return () => document.removeEventListener('click', handleClickOutside);
+  }, [dropdownOpen]);
+
   return (
-    <div id="sortOrderSelect">
+    <div id="sortOrderSelect" ref={dropdownRef}>
       <div id="sortOrder" css={style.sortOrder} onClick={toggleDropdown}>
         {viewport !== c.VIEWPORT.MOBILE && c.SORT_ORDER_MSG[sortOrder]}
         <Image
